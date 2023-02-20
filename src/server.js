3dcde84b6ecaef0d02d8c60688d0c77c86663aa6
Kazvalech <goh7675@rit.edu>
2023-02-19 21:52:46 -0500

    Add completed assignment

---
+++ src/server.js
@@ -0,0 +1,93 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// Recompiles the body of a request, and then calls the
+// appropriate handler once completed.
+// This function will reassemble the body and then handle the request.
+const parseBody = (request, response, handler) => {
+  // The request will come in in pieces. We will store those pieces in this
+  // body array.
+  const body = [];
+  // If there is an error, write it to the console and send
+  // back a 400-Bad Request error to the client.
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  // On receiving a chunk, put it into this array (always in order)
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  // After receiving all info, concat the body array into a string
+  // then into an object.
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    // Once we have the bodyParams object, we will call the handler function. We then
+    // proceed much like we would with a GET request.
+    handler(request, response, bodyParams);
+  });
+};
+
+// handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+  // If they go to /addUser
+  if (parsedUrl.pathname === '/addUser') {
+    // Call our below parseBody handler, and in turn pass in the
+    // jsonHandler.addUser function as the handler callback function.
+    parseBody(request, response, jsonHandler.addUser);
+  }
+};
+
+// handle GET requests
+const handleGet = (request, response, parsedUrl) => {
+  // route to correct method based on url
+  if (parsedUrl.pathname === '/') {
+    htmlHandler.getIndex(request, response);
+  } else if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else {
+    jsonHandler.notFound(request, response);
+  }
+};
+
+// handle HEAD requests
+const handleHead = (request, response, parsedUrl) => {
+  // route to correct method based on url
+  if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsersMeta(request, response);
+  } else {
+    jsonHandler.notFoundMeta(request, response);
+  }
+};
+
+const onRequest = (request, response) => {
+  // parse url into individual parts
+  // returns an object of url parts by name
+  const parsedUrl = url.parse(request.url);
+
+  // check if method was POST, otherwise assume GET
+  // for the sake of this example
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else if (request.method === 'HEAD') {
+    handleHead(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
